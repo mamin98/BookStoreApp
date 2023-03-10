37f7fa7d536d3486b2874a9d4265ae1f9816e0fa
mamin98 <mohamedamin1421@gmail.com>
2023-03-11 01:32:19 +0200

    update view model of book

---
+++ backend/Book Store/ViewModels/BookViewModel/HomeBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Book_Store.ViewModels.BookViewModel
+{
+    public class HomeBookViewModel
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Image { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
