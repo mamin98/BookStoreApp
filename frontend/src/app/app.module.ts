402780b99f7d17d91b1b4f959145b470864cd670
abdo adel <abdo48439@gmail.com>
2023-03-10 20:01:48 +0200

    add angular fiels

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AllBooksComponent } from './books/all-books/all-books.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartComponent } from './carts/cart/cart.component';
+import { CustomValidateComponent } from './authentication/custom-validate/custom-validate.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AllBooksComponent,
+    BookDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    CustomValidateComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/authentication/custom-validate/custom-validate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-validate',
+  templateUrl: './custom-validate.component.html',
+  styleUrls: ['./custom-validate.component.scss']
+})
+export class CustomValidateComponent {
+
+}
