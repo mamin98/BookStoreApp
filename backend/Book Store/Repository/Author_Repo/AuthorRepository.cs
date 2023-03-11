1517c90fc6875872d0aa68f51d7dd07703b0bb40
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-12 01:03:20 +0200

    Updating Author Interface & Repository

--- backend/Book Store/Repository/Author_Repo/AuthorRepository.cs
+++ backend/Book Store/Repository/Author_Repo/AuthorRepository.cs
@@ -20,7 +20,32 @@ namespace Book_Store.Repository.Author_Repo
         // Author by Id
         public Author GetById(int id)
         {
-            return context.Author.FirstOrDefault(b => b.Id == id);
+            return context.Author.FirstOrDefault(a => a.Id == id);
+        }
+
+        public void Insert(Author New)
+        {
+            context.Add(New);
+            context.SaveChanges();
+        }
+
+        public void Edit(Author author, int id)
+        {
+            Author ExistingAuthor = GetById(id);
+
+            ExistingAuthor = author;
+            context.Update(ExistingAuthor);
+
+            context.SaveChanges();
+        }
+
+        // Delete Author
+        public void Delete(int id)
+        {
+            Author author = GetById(id);
+
+            context.Remove(author);
+            context.SaveChanges();
         }
     }
 }
