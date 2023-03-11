821c0b4506eb6c6235c5bf7f07d0e2b96ad7823c
AbdoAdel <abdo48439@gmail.com>
2023-03-11 22:41:43 +0200

    Display all books with routerLink

--- frontend/src/app/books/all-books/all-books.component.ts
+++ frontend/src/app/books/all-books/all-books.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-all-books',
   templateUrl: './all-books.component.html',
   styleUrls: ['./all-books.component.scss']
 })
-export class AllBooksComponent {
-
+export class AllBooksComponent implements OnInit{
+books:any=[];
+constructor(private booksService:BooksService){}
+  ngOnInit(): void{
+    this.booksService.getAllBooks().subscribe(
+      data=>{
+        this.books=data;
+      }
+    )
+    // any thing return observable we can make subscribe
+  }
 }
--- frontend/src/app/books/services/books.service.ts
+++ frontend/src/app/books/services/books.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBooks } from '../IBooks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
+  baseUrl:string="/assets/data/books.json"
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getAllBooks():Observable<IBooks[]>{
+    return this.http.get<IBooks[]>(this.baseUrl);
+  }
 }
