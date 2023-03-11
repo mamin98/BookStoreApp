4cbed5991a6f3375b5f7c256cb1bbfd994fabbd5
AbdoAdel <abdo48439@gmail.com>
2023-03-11 22:26:03 +0200

    add modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllBooksComponent } from './books/all-books/all-books.component';
+import { CartComponent } from './carts/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home-allBooks",component:AllBooksComponent},
+  {path:"cart",component:CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllBooksComponent } from './books/all-books/all-books.component';
@@ -8,6 +10,7 @@ import { BookDetailsComponent } from './books/book-details/book-details.componen
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 import { CartComponent } from './carts/cart/cart.component';
 import { CustomValidateComponent } from './authentication/custom-validate/custom-validate.component';
 import { LoginComponent } from './authentication/login/login.component';
@@ -28,7 +31,11 @@ import { RegisterComponent } from './authentication/register/register.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
