41e0bb5919b57d75d65068f60fcea74b0e3ec310
AbdoAdel <abdo48439@gmail.com>
2023-03-11 22:46:01 +0200

    add routes for login and register pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
 import { AllBooksComponent } from './books/all-books/all-books.component';
 import { CartComponent } from './carts/cart/cart.component';
 
 const routes: Routes = [
   {path:"home-allBooks",component:AllBooksComponent},
-  {path:"cart",component:CartComponent}
+  {path:"cart",component:CartComponent},
+  // authentication Paths
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent}
+  // authentication Paths
 ];
 
 @NgModule({
