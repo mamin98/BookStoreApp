88170f8f017c4e86763868a42e93e35e611604fb
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-12 01:01:22 +0200

    Implementing Author DTOs "CreateAuthorDto"

---
+++ backend/Book Store/DTOs/AuthorDTOs/CreateAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store.DTOs.AuthorDTOs
+{
+    public class CreateAuthorDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Author's First Name")]
+        public string FirstName { get; set; } = "";
+
+        [Required(ErrorMessage = "Please Enter Author's Last Name")]
+        public string LastName { get; set; } = "";
+
+        public byte[]? UploadImg { get; set; }
+    }
+}
