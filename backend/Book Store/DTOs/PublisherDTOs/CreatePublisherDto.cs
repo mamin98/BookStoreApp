9a6f067e1479dd89a14e22b5f9cc79395bf6af55
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-13 14:21:40 +0200

    Implementing Publisher DTOs

---
+++ backend/Book Store/DTOs/PublisherDTOs/CreatePublisherDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store.DTOs.PublisherDTOs
+{
+    public class CreatePublisherDto
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
