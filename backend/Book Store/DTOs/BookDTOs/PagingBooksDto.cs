755573df42894b7ad790319acfda8c423e1dcb56
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-15 21:39:59 +0200

    Implementing PagingBooksDto

---
+++ backend/Book Store/DTOs/BookDTOs/PagingBooksDto.cs
@@ -0,0 +1,13 @@
+﻿using Book_Store.Models;
+
+namespace Book_Store.DTOs.BookDTOs
+{
+    public class PagingBooksDto
+    {
+        public List<Book> Books { get; set; } = new List<Book>();
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
