9eaabd17e222b34712bdffccf368a8c06976e477
AbdoAdel <abdo48439@gmail.com>
2023-03-15 12:06:42 +0200

    Add Selected Items To Cart Page

--- frontend/src/app/carts/cart/cart.component.ts
+++ frontend/src/app/carts/cart/cart.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  cartBooks:any[] = []
+
+  ngOnInit(): void {
+   this.getCartBooks();
+  }
+  getCartBooks(){
+    if("cart" in localStorage){
+      this.cartBooks = JSON.parse(localStorage.getItem("cart")!)
+     } //
+     console.log(this.cartBooks)
+  }
 
 }
