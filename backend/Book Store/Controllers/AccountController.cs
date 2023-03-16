3cea6aba9503c7dc6a1af5d669e34ee6093c8ddf
mamin98 <mohamedamin1421@gmail.com>
2023-03-16 20:48:42 +0200

    add identity for registeration , add user register dto

---
+++ backend/Book Store/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Book_Store.DTOs.AccountDTOs;
+using Book_Store.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        // declare Dependency Injecktion "DI"
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpGet("register")] // api/account/register
+        public async Task<IActionResult> Register(UserRegisterDto userDto) // RegisterUserDto it's class have registration data
+        {
+            if(ModelState.IsValid)
+            {
+                // create function don't take Dto, it's only take model so:
+                AppUser user = new AppUser();
+                user.UserName = userDto.userName;
+                user.Email = userDto.email;
+                user.PasswordHash = userDto.password;
+
+                // save datat in database, by using Store if result true will save data
+                // don't forget hash input password
+                IdentityResult result = await _userManager.CreateAsync(user, userDto.password);
+                
+                if (result.Succeeded)
+                {
+                    return Ok("Registered Successfully.");    //Accepted();
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            // if model is invalid
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ backend/Book Store/DTOs/AccountDTOs/UserRegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store.DTOs.AccountDTOs
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        [MinLength(3)]
+        public string userName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required]
+        [Compare("password")]
+        public string confirmPassword { get; set; }
+    }
+}
