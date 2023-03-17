2eba947be09f9cdcb5a2fb6032cee2948dfc7285
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-18 00:17:36 +0200

    Implementing TypeDto

---
+++ backend/Book Store/DTOs/TypeDTOs/TypeDto.cs
@@ -0,0 +1,11 @@
+﻿using Book_Store.DTOs.BookDTOs;
+
+namespace Book_Store.DTOs.TypeDTOs
+{
+    public class TypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public List<HomeBookDto> Books { get; set; } = new();
+    }
+}
