2cec4f90c7abf656ca42ce160ade9c6e21f0ad48
AbdoAdel <abdo48439@gmail.com>
2023-03-22 00:12:34 +0200

    Updating Style Books-Category

---
+++ frontend/src/app/books/ICategory.ts
@@ -0,0 +1,10 @@
+export interface ICategory{
+  id: number,
+  title: number,
+  image: string,
+  price: number,
+  quantity: number,
+  authorName: string,
+  publisherName: string,
+  category: null
+}
