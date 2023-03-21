bde92a279983e7f9e753925f1d1406e89754c14d
mamin98 <mohamedamin1421@gmail.com>
2023-03-21 21:29:12 +0200

    make authorize service as a guard

--- frontend/src/app/authentication/service/authorize.service.ts
+++ frontend/src/app/authentication/service/authorize.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { UsersService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorizeService {
+export class AuthorizeService implements CanActivate{
 
-  constructor() { }
+  constructor(private auth: UsersService, private router: Router) { }
 
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      alert("Sorry, you an unauthorized!")
+      this.router.navigate(['login']);
+      
+      return false;
+    }
+  }
   
   
 }
