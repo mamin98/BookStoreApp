22c20d2fa1caf20e218fd08e38613669202123ae
mamin98 <mohamedamin1421@gmail.com>
2023-03-23 08:35:28 +0200

    edit pattern for password

--- frontend/src/app/authentication/service/users.service.ts
+++ frontend/src/app/authentication/service/users.service.ts
@@ -28,7 +28,7 @@ export class UsersService {
       username: ['', [Validators.required, Validators.pattern("[a-zA-Z_]+")]],
       phone:['', [Validators.required, Validators.pattern("[0-9]{11}")]],
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]],
-      password: ['', [Validators.required, Validators.pattern('[^(?=.[a-z])(?=.[A-Z])(?=.*\d).{6,15}]')]],
+      password: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.-]*$')]],
       confirmpassword:['', Validators.required],
     },
     {validator:[confirmPassValidator]}
@@ -37,7 +37,7 @@ export class UsersService {
     // Login Form
     this.logform = this.formbuilder.group({
       username: ['', [Validators.required, Validators.pattern("[a-zA-Z_]+")]],
-      password: ['', [Validators.required, Validators.pattern('[^(?=.[a-z])(?=.[A-Z])(?=.*\d).{6,15}]')]]
+      password: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.-]*$')]]
     });
 
 
