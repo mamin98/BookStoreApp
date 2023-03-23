8ee78719e914441fc6f526c2b43f15120eb6b908
mamin98 <mohamedamin1421@gmail.com>
2023-03-23 02:13:36 +0200

    control admin role

--- frontend/src/app/authentication/RoleGuard/role.guard.ts
+++ frontend/src/app/authentication/RoleGuard/role.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UsersService } from '../service/users.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
+  
+  isAdmin: boolean = false;
+  role:string="";
+
+  constructor(private user: UsersService){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      
+      // get current tole
+      this.role = this.user.account_data.role;
+      if(this.role == "Administrator"){
+        this.isAdmin = true;
+      }
+
+      alert("Just allowed for Admins");
+      return this.isAdmin;
   }
   
 }
