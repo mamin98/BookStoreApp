52266bd30546b4adc01dd8c1638a362d6ee388d7
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-25 22:41:30 +0200

    Adding ImgFIle Property to HomeBookDto

--- backend/Book Store/DTOs/BookDTOs/HomeBookDto.cs
+++ backend/Book Store/DTOs/BookDTOs/HomeBookDto.cs
@@ -8,6 +8,8 @@
 
         public string? Image { get; set; }
 
+        public byte[]? ImgFile { get; set; }
+
         public decimal Price { get; set; }
 
         public int Quantity { get; set; }
