01ed82b3542ecf028e3f211cc2f263c5397efddb
Amr Abdelkamel <amromoorie@gmail.com>
2023-03-27 10:12:53 +0200

    Add free alternative to Ignite UI: - remove igniteui-webcomponents with related setup - setup ngx-bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RatingModule } from 'ngx-bootstrap/rating';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomValidateComponent } from './authentication/custom-validate/custom-validate.component';
@@ -12,16 +16,13 @@ import { RegisterComponent } from './authentication/register/register.component'
 import { AllBooksComponent } from './books/all-books/all-books.component';
 import { BookDetailsComponent } from './books/book-details/book-details.component';
 import { BookComponent } from './books/book/book.component';
+import { BooksWithCategoryComponent } from './books/books-with-category/books-with-category.component';
 import { CartComponent } from './cart/cart.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
-import { BooksWithCategoryComponent } from './books/books-with-category/books-with-category.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { AdminModule } from './admin/admin.module';
 
 @NgModule({
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
     AllBooksComponent,
@@ -46,6 +47,8 @@ import { AdminModule } from './admin/admin.module';
     ReactiveFormsModule,
     NgxPaginationModule,
     AdminModule,
+    BrowserAnimationsModule,
+    RatingModule.forRoot(),
   ],
   exports: [NgxPaginationModule],
   providers: [],
