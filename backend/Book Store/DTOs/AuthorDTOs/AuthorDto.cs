9c9a1f67c0868dbd522e6ef9cd54739726d6149e
Kareem <kareemeldessoukycivil@gmail.com>
2023-03-27 17:45:03 +0200

    Implementing AuthorDto

---
+++ backend/Book Store/DTOs/AuthorDTOs/AuthorDto.cs
@@ -0,0 +1,16 @@
+﻿using Book_Store.DTOs.BookDTOs;
+
+namespace Book_Store.DTOs.AuthorDTOs
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string? Image { get; set; }
+
+        public byte[]? UploadImg { get; set; }
+
+        public List<HomeBookDto> Books { get; set; }
+    }
+}
