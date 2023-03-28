20b60cff0c37c71eb46f5d285c1cb4d91ea3429b
Amr Abdelkamel <amromoorie@gmail.com>
2023-03-28 21:11:55 +0200

    chore: - include checkout module - clean unnessary imports - test newly checkout data ready for updated styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { CartComponent } from './cart/cart.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
     HeaderComponent,
     FooterComponent,
     CartComponent,
+    CheckoutComponent,
     CustomValidateComponent,
     LoginComponent,
     RegisterComponent,
